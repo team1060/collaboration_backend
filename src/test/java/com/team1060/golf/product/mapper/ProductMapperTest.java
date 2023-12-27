fbc1553393c78a795cd05cf8ee52ae76c93c86ed
team1060 <team1060dev@gmail.com>
2023-12-27 21:48:23 +0900

    Feat: CrawlTest 작성

--- src/test/java/com/team1060/golf/product/mapper/ProductMapperTest.java
+++ src/test/java/com/team1060/golf/product/mapper/ProductMapperTest.java
@@ -4,14 +4,17 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Log4j2
-@SpringBootTest
 public class ProductMapperTest {
 	
 	@Autowired
